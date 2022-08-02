71b68b74a92d4930ce2761bd454a4307b7d7d466
Gyuricza Ádám <suhf.adam@gmail.com>
2022-08-02 12:17:11 +0200

    age screen implemented

--- onboarding/onboarding_presentation/src/main/java/hu/adikaindustries/onboarding_presentation/age/AgeViewModel.kt
+++ onboarding/onboarding_presentation/src/main/java/hu/adikaindustries/onboarding_presentation/age/AgeViewModel.kt
@@ -4,12 +4,17 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import hu.adikaindustries.core.R
 import dagger.hilt.android.lifecycle.HiltViewModel
 import hu.adikaindustries.core.domain.preferences.Preferences
 import hu.adikaindustries.core.domain.use_case.FilterOutDigits
+import hu.adikaindustries.core.navigation.Route
 import hu.adikaindustries.core.util.UIEvent
+import hu.adikaindustries.core.util.UiText
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -29,4 +34,16 @@ class AgeViewModel @Inject constructor(
         }
     }
 
+    fun onNextClick(){
+        viewModelScope.launch {
+            val ageNumber = age.toIntOrNull() ?:kotlin.run {
+                _uiEvent.send(
+                    UIEvent.ShowSnackBar(UiText.StringResource(R.string.error_age_cant_be_empty))
+                )
+                return@launch
+            }
+            preferences.saveAge(ageNumber)
+            _uiEvent.send(UIEvent.Navigate(Route.HEIGHT))
+        }
+    }
 }
