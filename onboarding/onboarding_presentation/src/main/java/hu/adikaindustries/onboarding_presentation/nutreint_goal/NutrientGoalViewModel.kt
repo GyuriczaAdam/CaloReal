847704b75ce1a05ef6c6cf3d92b7f3edcb79c39d
Gyuricza Ádám <suhf.adam@gmail.com>
2022-08-03 20:15:37 +0200

    validate nutrient usecase added with viewModel

--- onboarding/onboarding_presentation/src/main/java/hu/adikaindustries/onboarding_presentation/nutreint_goal/NutrientGoalViewModel.kt
+++ onboarding/onboarding_presentation/src/main/java/hu/adikaindustries/onboarding_presentation/nutreint_goal/NutrientGoalViewModel.kt
@@ -4,18 +4,22 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import hu.adikaindustries.core.domain.preferences.Preferences
 import hu.adikaindustries.core.domain.use_case.FilterOutDigits
 import hu.adikaindustries.core.util.UIEvent
+import hu.adikaindustries.onboarding_domain.use_case.ValidateNutrients
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class NutrientGoalViewModel @Inject constructor(
     private val preferences: Preferences,
     private val filterOutDigits: FilterOutDigits,
+    private val validateNutrients: ValidateNutrients
 ): ViewModel() {
 
     var state by mutableStateOf(NutrientGoalState())
@@ -41,9 +45,24 @@ class NutrientGoalViewModel @Inject constructor(
                 )
             }
             is NutrientGoalEvent.OnNextClick->{
-                state = state.copy(
+               val result = validateNutrients(
+                   carbRatioText = state.carbsRatio,
+                   fatRatioText = state.fatRatio,
+                   proteinRatioText = state.protenRatio
+                   )
+                when(result){
+                    is ValidateNutrients.Result.Success->{
+                        preferences.saveCarbRatio(result.carbsRatio)
+                        preferences.saveFatRatio(result.fatRatio)
+                        preferences.saveProteinRatio(result.proteinRatio)
 
-                )
+                    }
+                    is ValidateNutrients.Result.Error->{
+                        viewModelScope.launch {
+                            _uiEvent.send(UIEvent.ShowSnackBar(result.message))
+                        }
+                    }
+                }
             }
         }
     }
