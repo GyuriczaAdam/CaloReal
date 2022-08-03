2a642943fa437e00596c68f877a05e59c852f556
Gyuricza Ádám <suhf.adam@gmail.com>
2022-08-03 21:20:55 +0200

    tracker database implemented

--- tracker/tracker_data/src/main/java/hu/adikaindustries/tracker_data/local/Database/TrackerDatabase.kt
+++ tracker/tracker_data/src/main/java/hu/adikaindustries/tracker_data/local/Database/TrackerDatabase.kt
@@ -1,9 +1,11 @@
 package hu.adikaindustries.tracker_data.local.Database
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
+import hu.adikaindustries.tracker_data.local.DAO.TrackerDAO
 import hu.adikaindustries.tracker_data.local.entity.TrackedFoodEntity
 
 @Database(entities = [TrackedFoodEntity::class], version = 1)
-abstract class TrackerDatabase {
-
+abstract class TrackerDatabase: RoomDatabase() {
+    abstract val dao:TrackerDAO
 }
