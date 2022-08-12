94407cb66633e97530f628c624381baa28ffa3a6
Gyuricza Ádám <suhf.adam@gmail.com>
2022-08-12 12:18:03 +0200

    tracked food item added

--- tracker/tracker_presentation/src/main/java/hu/adikaindustries/tracker_presentation/tracker_overview/TrackerOverViewScreen.kt
+++ tracker/tracker_presentation/src/main/java/hu/adikaindustries/tracker_presentation/tracker_overview/TrackerOverViewScreen.kt
@@ -49,7 +49,7 @@ fun TrackerOverViewScreen(
                 meal = meal,
                 onToggleClick = {viewModel.onEvent(TrackerOverViewEvent.OnToggleMealClick(meal))},
                 content = {
-
+                    
                 },
                 modifier = Modifier.fillMaxWidth()
                 )
